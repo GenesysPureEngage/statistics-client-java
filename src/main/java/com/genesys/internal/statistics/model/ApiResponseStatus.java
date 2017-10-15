d9687698977ba549cf61f3380d3b8c2e1d3518af
maksimlysenko <maxim.lysenko@genesys.com>
2017-10-15 19:30:50 +0000

    9.0.000.00.562

--- src/main/java/com/genesys/internal/statistics/model/ApiResponseStatus.java
+++ src/main/java/com/genesys/internal/statistics/model/ApiResponseStatus.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 /**
  * ApiResponseStatus
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2017-10-15T18:33:25.601Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2017-10-15T19:04:21.713Z")
 public class ApiResponseStatus {
   @SerializedName("code")
   private Integer code = null;
--- src/main/java/com/genesys/internal/statistics/model/StatisticsData.java
+++ src/main/java/com/genesys/internal/statistics/model/StatisticsData.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 /**
  * StatisticsData
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2017-10-15T18:33:25.601Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2017-10-15T19:04:21.713Z")
 public class StatisticsData {
   @SerializedName("data")
   private StatisticsData data = null;
