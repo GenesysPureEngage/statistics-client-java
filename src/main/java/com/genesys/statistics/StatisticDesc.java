84c83d83679cee7285d246cfa11199ef367cb64c
witchkun <nyatta.hen@gmail.com>
2017-10-28 00:57:02 +0300

    add wrapper for generated classes

---
+++ src/main/java/com/genesys/statistics/StatisticDesc.java
@@ -0,0 +1,33 @@
+package com.genesys.statistics;
+
+public class StatisticDesc extends StatisticInfo {
+    private String statisticId;
+    private Object definition;
+
+    public StatisticDesc(String statisticId, String objectId, String objectType, String name) {
+        this(statisticId, objectId, objectType, name, null);
+    }
+    
+    public StatisticDesc(String statisticId, String objectId, String objectType, Object definition) {
+        this(statisticId, objectId, objectType, null, definition);
+    }
+
+    private StatisticDesc(String statisticId, String objectId, String objectType, String name, Object definition) {
+        super(objectId, objectType, name);
+        
+        this.statisticId = statisticId;
+        this.definition = definition;
+    }
+
+    public String getStatisticId() {
+        return statisticId;
+    }
+    
+    public Object getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(Object definition) {
+        this.definition = definition;
+    }
+}
---
+++ src/main/java/com/genesys/statistics/StatisticsException.java
@@ -0,0 +1,11 @@
+package com.genesys.statistics;
+
+public class StatisticsException extends Exception {
+    public StatisticsException(String msg) {
+        super(msg);
+    }
+
+    public StatisticsException(String msg, Exception cause) {
+        super(msg, cause);
+    }
+}
