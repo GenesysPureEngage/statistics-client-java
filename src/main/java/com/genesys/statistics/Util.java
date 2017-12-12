cadd9e903629bb6d171caf6b348dcda6937a012f
maksimlysenko <Maxim.Lysenko@genesys.com>
2017-12-12 11:07:31 -0800

    Synchronized library code with SDKs update

--- src/main/java/com/genesys/statistics/Util.java
+++ src/main/java/com/genesys/statistics/Util.java
@@ -1,13 +1,13 @@
 package com.genesys.statistics;
 
-import com.genesys.internal.statistics.model.ApiResponseStatus;
+import com.genesys.internal.statistics.model.ResponseStatus;
 
 public class Util
 {
 	private static final int STATUS_ASYNC_OK = 1;
 	private static final int STATUS_OK = 0;
 
-	public static void throwIfNotOk(ApiResponseStatus status) throws StatisticsException
+	public static void throwIfNotOk(ResponseStatus status) throws StatisticsException
 	{
 		Integer code = status.getCode();
 		if (code != STATUS_ASYNC_OK && code != STATUS_OK)
