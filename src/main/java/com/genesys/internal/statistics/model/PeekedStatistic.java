b51e9c6e5a12d9ddd0e2e1921c45136a18e9eaf4
maksimlysenko <maxim.lysenko@genesys.com>
2018-04-25 17:30:08 +0000

    9.0.000.00.968

--- src/main/java/com/genesys/internal/statistics/model/PeekedStatistic.java
+++ src/main/java/com/genesys/internal/statistics/model/PeekedStatistic.java
@@ -27,7 +27,7 @@ import java.io.IOException;
 /**
  * PeekedStatistic
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-04-25T16:47:31.631Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-04-25T17:21:29.731Z")
 public class PeekedStatistic {
   @SerializedName("statistic")
   private PeekedStatisticValue statistic = null;
--- src/main/java/com/genesys/internal/statistics/model/StatisticDataResponse.java
+++ src/main/java/com/genesys/internal/statistics/model/StatisticDataResponse.java
@@ -31,7 +31,7 @@ import java.util.List;
 /**
  * StatisticDataResponse
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-04-25T16:47:31.631Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-04-25T17:21:29.731Z")
 public class StatisticDataResponse {
   @SerializedName("data")
   private StatisticData data = null;
