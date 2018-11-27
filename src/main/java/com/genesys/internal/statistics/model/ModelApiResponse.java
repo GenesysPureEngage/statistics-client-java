0af5bf3964f18217e9c81823b9e04d2d2c396af0
maksimlysenko <maxim.lysenko@genesys.com>
2018-11-27 02:28:09 +0000

    Release: 9.0.2

--- src/main/java/com/genesys/internal/statistics/model/ModelApiResponse.java
+++ src/main/java/com/genesys/internal/statistics/model/ModelApiResponse.java
@@ -30,7 +30,7 @@ import java.util.List;
 /**
  * ModelApiResponse
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-11-20T20:48:45.541Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-11-23T01:19:53.694Z")
 public class ModelApiResponse {
   @SerializedName("data")
   private Object data = null;
--- src/main/java/com/genesys/internal/statistics/model/PeekedStatisticValue.java
+++ src/main/java/com/genesys/internal/statistics/model/PeekedStatisticValue.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 /**
  * PeekedStatisticValue
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-11-20T20:48:45.541Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-11-23T01:19:53.694Z")
 public class PeekedStatisticValue {
   @SerializedName("name")
   private String name = null;
